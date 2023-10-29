b737b51b26f15d8f242a33df7034cbcc49a0e203
MitchZas <102780399+MitchZas@users.noreply.github.com>
2023-10-29 12:51:04 -0400

    Revert "Pulling for latest!"

--- CClimber/Assets/Scripts/PlayerMovement.cs
+++ CClimber/Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -11,35 +9,10 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D rb2d;
     public float jump;
-
-    private bool previousGrounded;
-    private bool playerGrounded;
-
-    public Vector2 boxSize;
-    public float castDistance;
-    public LayerMask groundLayer;
-    
-    public AudioClip clipJump;
-    public AudioClip clipLand;
-    AudioSource audioSource;
     
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        audioSource = GetComponent<AudioSource>();
-        previousGrounded = false;
-        playerGrounded = false;
-    }
-
-    bool isGrounded(){
-        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer)){
-            return true;
-        }
-        return false;
-    }
-
-    public void OnDrawGizmos(){
-        Gizmos.DrawWireCube(transform.position - transform.up * castDistance, boxSize);
     }
 
     // Update is called once per frame
@@ -51,23 +24,9 @@ public class PlayerMovement : MonoBehaviour
 
         transform.Translate(new Vector3(horizontal, 0f, 0f));
 
-        
-        // Character jump
-        if(isGrounded())
+        if(Input.GetButtonDown("Jump"))
         {
-            if(!previousGrounded){
-                audioSource.PlayOneShot(clipLand, 1);
-            }
-            previousGrounded = true;
-
-            if(Input.GetButtonDown("Jump") ){
-                Debug.Log("Jump!");
-                rb2d.AddForce(new Vector2(rb2d.velocity.x, jump));
-                audioSource.PlayOneShot(clipJump, 1);
-            }
-        }
-        else{
-            previousGrounded = false;
+            rb2d.AddForce(new Vector2(rb2d.velocity.x, jump));
         }
     }
 }
