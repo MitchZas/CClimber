9a97dbbfce06840169e0b2a078cdea877c23041c
MitchZas <mitchellzasa@gmail.com>
2023-10-29 13:45:24 -0400

    Created Falling Platforms

--- CClimber/Assets/FallingPlatform.cs
+++ CClimber/Assets/FallingPlatform.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class FallingPlatform : MonoBehaviour
 {
-    private float fallDelay;
-    
-    // Start is called before the first frame update
-    void Start()
+    private float fallDelay = 1f;
+    private float destroyDelay = 2f;
+
+    [SerializeField] private Rigidbody2D rb;
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(Fall());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Fall()
     {
-        
+        yield return new WaitForSeconds(fallDelay);
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        Destroy(gameObject, destroyDelay);
     }
 }
